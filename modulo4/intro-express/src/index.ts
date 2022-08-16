adc745697bb34ac0769ce73c5633025d16339ce8
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-08-15 21:06:32 -0300

    2 questoes resolvidas

--- modulo4/intro-express/src/index.ts
+++ modulo4/intro-express/src/index.ts
@@ -1,10 +1,48 @@
 import express, {Express} from 'express'
 import cors from 'cors'
+import { AddressInfo } from "net";
 
 const app: Express = express();
 
 app.use(express.json());
 app.use(cors());
 
-// a função express() inicia a aplicação web com express
-// os .use() ativam os módulos de Bodyparser e Cors
+
+// 1)
+// app.get('/', (req, res) => {
+//     res.send('Hello, world!')
+//    })
+
+// 2) 
+// app.get('/users', (req, res) => {
+//     if (req.headers.chave === "Freire"){
+//         res.send(`{
+//             "id": 1,
+//             "name": "Leanne Graham",
+//             "phone": "82 8747-89842",
+//             "email": "Sincere@april.biz",
+//             "website": "writing-landing.com"
+//         }`)
+//     } else {
+//         res.send("Valor não encontrado")
+//     }
+    
+//    })
+
+const server = app.listen(process.env.PORT || 3003, () => {
+  if (server) {
+    const address = server.address() as AddressInfo;
+    console.log(`Servidor rodando na porta 3003: http://localhost:${address.port}`);
+  } else {
+    console.error(`Falhou ao iniciar o Servidor.`);
+  }
+});;
+
+
+// {
+//     "id": 1,
+//     "name": "Leanne Graham",
+//     "phone": "82 8747-89842",
+//     "email": "Sincere@april.biz",
+//     "website": "writing-landing.com"
+//       }
