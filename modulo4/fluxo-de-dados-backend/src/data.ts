09e5d6dc0fc50a0cdeafab36c9b663e3d06df36c
Gustavo Monteiro de Assunção <monteirocontacto@gmail.com>
2022-08-17 18:22:29 -0300

    praticando fluxo de dados

---
+++ modulo4/fluxo-de-dados-backend/src/data.ts
@@ -0,0 +1,23 @@
+type product = {
+   id: string, 
+   name: string,
+   price: number
+}
+
+export const products:product[] = [
+   {
+      id: '1',
+      name: 'Shoes',
+      price: 100.00
+   },
+   {
+      id: '2',
+      name: 'Bags',
+      price: 500.00
+   },
+   {
+      id: '3',
+      name: 'Hats',
+      price: 50.00
+   },    
+];
